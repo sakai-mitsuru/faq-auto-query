3a762ba8b3ee8e874f155cc4f88666b32139aaf3
sakai mitsuru <sakai.mitsuru@toshiba-sol.co.jp>
2017-03-16 15:11:13 +0900

    unify index.js between faq and persondirectory

--- index.js
+++ index.js
@@ -2,25 +2,25 @@
 var PythonShell = require('python-shell');
 var PY_FILE_NAME = 'query_make_t_bkw3.py';
 
-// var run = (pyfilepath) =>{
-//   PythonShell.run(pyfilepath, (err,results)=> {
-//     if (err) throw err;
-//     console.log(results);
-//     console.log('finished');
-//   });
-// }
-
+/**
+ * promisified
+ */
 const pShellOn = (pyfilepath,msg) => {
   return new Promise((resolve,reject)=>{
     shellOn(resolve,reject,pyfilepath,msg)
   });
 }
 
+/**
+ * execute py on pyhon shell
+ * @param resolve {function} func on success
+ * @param reject  {function} func on error
+ * @param pyfilepath {string} python script file path
+ * @param msg   {string}  original message for retrieve
+ * @return {[string]} keywords ["keyword1","keyword2",....]
+ */
 const shellOn = (resolve,reject,pyfilepath,msg) =>{
-  // var pyShell = new PythonShell(pyfilepath, {mode : 'json', args : [msg] });
   var pyShell = new PythonShell(pyfilepath, {mode : 'text', args : [msg] ,pythonPath : 'python27.exe'});
-  //var pyShell = new PythonShell(pyfilepath, {mode : 'json', args : [msg] ,pythonPath : 'python27.exe'});
-  // var pyShell = new PythonShell(pyfilepath, {mode : 'json' });
   pyShell.on('message',(result)=>{
     //console.log(result)
     resolve(result);
@@ -30,18 +30,33 @@ const shellOn = (resolve,reject,pyfilepath,msg) =>{
   })
 }
 
+// entry point
+const main = (msg_) =>{
+  let outAry = [];
+  pShellOn(PY_FILE_NAME,msg_)
+    .then((retAry)=>{
+      console.log(retAry);
+    })
+    .catch((err)=>{
+      console.dir(err);
+    });
+}
+
+// export module
+module.exports = pShellOn;
+
 // debug -------------------------
 // -------------------------------
 if(require.main === module){
-  var main = () =>{
-    var msg_ = 'Excelについて教えて';
-    pShellOn(PY_FILE_NAME,msg_)
-      .then((result)=>{
-        console.log(result);
-      })
-      .catch((err)=>{
-        console.dir(err);
-      });
+  // debug sample
+  var sample = () =>{
+    var file_path = PY_FILE_NAME
+    run(file_path);
   }
-  main();
+  //sample();
+
+  // debug execution
+  // const msg = 'Excel';
+  const msg = 'Excelに詳しい人教えて';
+  main(msg);
 }
